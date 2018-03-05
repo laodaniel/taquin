3841076ecd0265f476bd7a08e632a5a9567958cd
Daniel Lao <laodaniel@gmail.com>
2018-03-05 16:39:47 +0100

    Shuffle the board

--- src/board.js
+++ src/board.js
@@ -4,7 +4,10 @@ import Piece from './piece';
 
 const PIECES_COUNT = 25;
 const COLUMNS_COUNT = Math.sqrt(PIECES_COUNT);
-const ROW_HEIGHT = `${100/COLUMNS_COUNT}vw`;
+const ROW_HEIGHT = `${100 / COLUMNS_COUNT}vw`;
+
+const board = Array.from(Array(PIECES_COUNT).keys());
+const shuffledBoard = board.sort(() => Math.random() - 0.5);
 
 class Board extends Component {
   componentDidMount() {
@@ -12,17 +15,14 @@ class Board extends Component {
       '--columns-count',
       COLUMNS_COUNT,
     );
-    document.documentElement.style.setProperty(
-      '--row-height',
-      ROW_HEIGHT,
-    );
+    document.documentElement.style.setProperty('--row-height', ROW_HEIGHT);
   }
 
   render() {
     return (
       <section className={styles.board}>
-        {Array.from(Array(PIECES_COUNT).keys()).map((piece, index) => (
-          <Piece key={index} number={index} />
+        {shuffledBoard.map((piece, index) => (
+          <Piece key={index} number={piece} />
         ))}
       </section>
     );
