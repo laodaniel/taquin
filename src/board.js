96231dccb13be22650450f64cf1d2f57e7d3e632
Daniel Lao <laodaniel@gmail.com>
2018-03-06 09:54:01 +0100

    Add a solved state with a restart button

--- src/board.js
+++ src/board.js
@@ -2,19 +2,19 @@ import React, {Component} from 'react';
 import styles from './styles.css';
 import Tile from './tile';
 
-const TILES_COUNT = 25;
+const TILES_COUNT = 16;
 const COLUMNS_COUNT = Math.sqrt(TILES_COUNT);
 const ROW_HEIGHT = `${100 / COLUMNS_COUNT}vw`;
 const HOLE_VALUE = 0;
 
 const board = Array.from(Array(TILES_COUNT).keys());
-const shuffledBoard = board.sort(() => Math.random() - 0.5);
 
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      shuffledBoard,
+      shuffledBoard: this.shuffleBoard(),
+      isSolved: false,
     };
   }
 
@@ -25,6 +25,12 @@ class Board extends Component {
     );
     document.documentElement.style.setProperty('--row-height', ROW_HEIGHT);
   }
+
+  shuffleBoard = () => {
+    const shuffledBoard = [...board];
+    shuffledBoard.sort(() => Math.random() - 0.5);
+    return shuffledBoard;
+  };
 
   onTileClick = event => {
     const value = parseInt(event.currentTarget.innerHTML, 10);
@@ -67,13 +73,32 @@ class Board extends Component {
     shuffledBoard[this.getTileIndex(value1)] = value2;
     shuffledBoard[this.getTileIndex(value2)] = value1;
     this.setState({shuffledBoard});
+    this.checkSolvedState();
+  };
+
+  checkSolvedState = () => {
+    if (JSON.stringify(this.state.shuffledBoard) === JSON.stringify(board)) {
+      this.setState({isSolved: true});
+    }
+  };
+
+  restart = () => {
+    this.setState({isSolved: false, shuffledBoard: this.shuffleBoard()});
   };
 
   getTileIndex = tileValue =>
     this.state.shuffledBoard.findIndex(value => value === tileValue);
 
   render() {
-    const {shuffledBoard} = this.state;
+    const {shuffledBoard, isSolved} = this.state;
+    if (isSolved) {
+      return (
+        <section>
+          <h1>You win!</h1>
+          <button onClick={this.restart}>Restart</button>
+        </section>
+      );
+    }
     return (
       <section className={styles.board}>
         {shuffledBoard.map(tile => (
